29d87a06348143934c87d3a0245bc303a977c251
jhoanmunoz24 <pirrijhoan@gmail.com>
2026-03-05 22:17:08 -0500

    Backend fundionando correctamente

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import test_endpoint, ActividadViewSet, subtareas_list, actividades_hoy
+from .views import test_endpoint, ActividadViewSet, subtareas_list, subtarea_detail, actividades_hoy
 
 router = DefaultRouter()
 router.register(r'actividades', ActividadViewSet)
@@ -9,5 +9,6 @@ urlpatterns = [
     path('health/', test_endpoint),
     path('actividades/hoy/', actividades_hoy, name='actividades-hoy'),
     path('actividades/<int:actividad_id>/subtareas/', subtareas_list, name='subtareas-list'),
+    path('subtareas/<int:pk>/', subtarea_detail, name='subtarea-detail'),
     path('', include(router.urls)),
 ]
